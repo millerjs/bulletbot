2b8dee35fe899bdf2c4d326acfd6a4c615ab7dc8
Joshua Miller <jshuasmiller@gmail.com>
2016-01-02 20:31:49 -0600

    add email scheduler

--- bulletbot/bulletbot.py
+++ bulletbot/bulletbot.py
@@ -9,9 +9,16 @@ Defines :class:`.BulletBot`.
 
 import configargparse
 import logging
+import markovify
 import re
+import smtplib
 import sqlalchemy as sa
 import textwrap
+import time
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from email.mime.text import MIMEText
+from getpass import getpass
 
 from .driver import SQLAlchemyDriver
 
@@ -83,6 +90,17 @@ class BulletBot(object):
         parser.add('-u', '--user', env_var='BBOT_USER', required=True)
         parser.add('-p', '--password', env_var='BBOT_PASS', required=True)
         parser.add('-t', '--token', env_var='BBOT_SLACK_TOKEN')
+
+        parser.add('--email-user', env_var='BBOT_EMAIL_USER')
+        parser.add('--email-from', env_var='BBOT_EMAIL_FROM')
+        parser.add('--email-to', env_var='BBOT_EMAIL_TO')
+        parser.add('--email-server', env_var='BBOT_EMAIL_SERVER')
+        parser.add('--email-pass', env_var='BBOT_EMAIL_PASS')
+        parser.add('--email-port', env_var='BBOT_EMAIL_PORT',
+                   type=int, default=587)
+        parser.add('--cron-hour', env_var='BBOT_CRON_HOUR')
+        parser.add('--cron-minute', env_var='BBOT_CRON_MINUTE')
+
         return parser
 
     @staticmethod
@@ -100,6 +118,17 @@ class BulletBot(object):
         """
 
         return [index.strip() for index in re.split(delim, text)]
+
+    def markov_nick(self, nick):
+        with self.db.session() as s:
+            user = s.query(User).filter(User.nick == nick).first()
+            if not user:
+                return None
+            else:
+                bullets = [b.bullet for b in user.bullets]
+
+        model = markovify.Text('\n'.join(bullets))
+        return model.make_sentence(retries=100)
 
     def merge_nick(self, nick, realname=None):
         """If no :class:`.models.User` entry with `nick` exists in the
@@ -171,6 +200,7 @@ class BulletBot(object):
         bullet.nick = nick
         with self.db.session() as s:
             s.merge(bullet)
+
         response = 'Wrote bullet: {}'.format(bullet.bullet)
 
         self.logger.info((nick, response))
@@ -431,3 +461,66 @@ class BulletBot(object):
                     .filter(Bullet.last_sent == None)  # noqa
                     .update({Bullet.last_sent: sa.func.now()},
                             synchronize_session=False))
+
+    def send_bullets(self, message=None):
+        """Sends bullets to recipient per config specification.  If
+        :param:`message` is provided, send this message instead.
+
+        """
+        msg = MIMEText(message or self.compile_plaintext_bullets())
+
+        assert self.args.email_user, 'No email user specified'
+        assert self.args.email_server, 'No email server specified'
+        assert self.args.email_to, 'No email recip specified'
+        assert self._email_password, 'No email pass specified'
+
+        msg['Subject'] = 'Bullets {}'.format(time.strftime("%m-%d-%Y"))
+        msg['From'] = self.args.email_from
+        msg['To'] = self.args.email_to
+
+        print('Connecting to {}'.format(self.args.email_server))
+        server = smtplib.SMTP(self.args.email_server)
+        server.starttls()
+        server.ehlo()
+        server.login(self.args.email_user, self._email_password)
+
+        server.sendmail(
+            self.args.email_from,
+            [self.args.email_to],
+            msg.as_string()
+        )
+
+        self.logger.info(msg)
+
+    def send_bullets_mark_sent(self):
+        """Send bullets and mark all unsent bullets sent.
+
+        """
+
+        self.send_bullets()
+        self.mark_all_sent()
+
+    def schedule_send_bullets(self):
+        """Blocking call to schedule bullets.
+
+        """
+
+        self._email_password = (
+            self.args.email_pass
+            or getpass('Email password for {}: '.format(self.args.email_user))
+        )
+        cron_args = {k: v for k, v in dict(
+            hour=self.args.cron_hour,
+            minute=self.args.cron_minute,
+        ).items() if v}
+
+        assert cron_args, 'No cron args specified.'
+
+        scheduler = BlockingScheduler()
+        scheduler.add_job(self.send_bullets_mark_sent, 'cron', **cron_args)
+
+        self.logger.info("Scheduled for {}".format(cron_args))
+        try:
+            scheduler.start()
+        except (KeyboardInterrupt, SystemExit):
+            raise
